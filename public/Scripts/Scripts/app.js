9fb93ce1af2247c159694edca418aa522d39e15b
Sohail-OTU <sohail.baig@ontariotechu.net>
2024-11-05 21:33:41 -0500

    node js update3

---
+++ public/Scripts/Scripts/app.js
@@ -0,0 +1,11 @@
+/* SPA --> Single Page Application */
+// Client Side
+// IIFE --> Immediately Invoked Function Expression
+
+(function(){
+    function Start()
+    {
+        console.log("Application has started...");
+    }
+    window.addEventListener("load", Start);
+})();
